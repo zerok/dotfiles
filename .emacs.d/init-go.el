da383cae520868c479ab42f4228570b15f4faa6e
Horst Gutmann <zerok@zerokspot.com>
2015-07-17 19:26:40 +0200

    Cleanup, emojis and elixir

--- .emacs.d/init-go.el
+++
@@ -1,8 +0,0 @@
-(setq gofmt-command "goimports")
-
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-(add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (company-mode)))
-
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,20 @@
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin:/Users/zerok/go/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin" "/Users/zerok/go/bin")))
 (setenv "GOPATH" "/Users/zerok/go")
-(setq default-buffer-file-coding-system 'utf-8-unix)
 
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 ;; Make sure that all the required packages are installed
-(dolist (package '(ag
+(dolist (package '(
+		           ag
+                   alchemist
                    company
                    company-go
-                   flx
-                   flx-ido
+                   elixir-mode
                    flycheck
+                   flycheck-rust
                    go-mode
                    haskell-mode
                    helm
@@ -22,18 +23,23 @@
                    htmlize
                    js2-mode
                    jsx-mode
+                   rust-mode
                    magit
                    markdown-mode
                    neotree
                    org
                    projectile
+                   jedi
+                   company-jedi
                    scss-mode
                    smyx-theme
                    tern
                    yaml-mode
+                   unicode-fonts
                    evil
                    evil-leader
-                   evil-commentary))
+                   evil-commentary
+                   ))
   (when (not (package-installed-p package))
     (package-install package)
     )
@@ -47,7 +53,8 @@
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("f9e975bdf5843982f4860b39b2409d7fa66afab3deb2616c41a403d788749628" default)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "0f002f8b472e1a185dfee9e5e5299d3a8927b26b20340f10a8b48beb42b55102" "f9e975bdf5843982f4860b39b2409d7fa66afab3deb2616c41a403d788749628" default)))
+ '(fci-rule-color "#151515")
  '(flycheck-checkers
    (quote
     (css-csslint emacs-lisp emacs-lisp-checkdoc go-gofmt go-golint go-vet go-build go-test go-errcheck haskell-ghc haskell-hlint html-tidy javascript-eslint javascript-jscs javascript-jshint json-jsonlint less make perl perl-perlcritic python-flake8 python-pylint python-pycompile rst rst-sphinx ruby-rubylint ruby ruby-jruby rust sass scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck tex-chktex tex-lacheck texinfo xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby)))
@@ -55,53 +62,29 @@
  '(js3-consistent-level-indent-inner-bracket t)
  '(js3-enter-indents-newline t)
  '(js3-indent-level 4)
- '(line-spacing 3)
+ '(line-spacing 4)
  '(ns-antialias-text t)
  '(ns-command-modifier (quote meta))
+ '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(size-indication-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(unicode-fonts-skip-font-groups (quote (decorative low-quality-glyphs))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282828" :foreground "#e5e5e5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "nil" :family "Monaco")))))
-
-;; Set the hard-wrapping column to 80 to be more in line with coding guidelines
-;; like PEP8.
-(setq flycheck-idle-change-delay 2)
-(setq company-dabbrev-downcase nil)
-
-
-;; Custom mode assignments
-(add-to-list 'auto-mode-alist '("\\.zshrc\\.(local|private)\\'". sh-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Global modes to be enabled everywhere
-(add-hook 'after-init-hook #'global-flycheck-mode) ;; static code checking
-(add-hook 'after-init-hook 'global-company-mode) ;; auto completion
-(windmove-default-keybindings) ;; better window navigation
+ '(default ((t (:inherit nil :stipple nil :background "#282828" :foreground "#e5e5e5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Inconsolata for Powerline")))))
+;;(setq flycheck-idle-change-delay 2)
 
 ;; https://github.com/topfunky/PeepOpen-Issues/issues/13
 (setq ns-pop-up-frames nil)
 
-;; Allow some features that are by default disabled
-(put 'narrow-to-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-
-;; http://emacswiki.org/emacs/ShowParenMode
-(show-paren-mode 1)
-
-(set-terminal-coding-system 'utf-8-unix)
-(require 'epa-file)
-(epa-file-enable)
-
-(server-start)
-(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
-
+;; (set-terminal-coding-system 'utf-8-unix)
+;; (setq default-buffer-file-coding-system 'utf-8-unix)
 
 (require 'org)
 (load-file "~/.emacs.d/private.el")
 (org-babel-load-file "~/.emacs.d/settings.org")
-
+(add-hook 'after-init-hook 'global-company-mode)
+(require 'elixir-mode)
+(require 'alchemist)
+(server-start)
+(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
 ;;; init.el ends here
