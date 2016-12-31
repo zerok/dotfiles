26cc0efaad41106818bfcaa7760d56789e88e91a
Horst Gutmann <horst@zerokspot.com>
2016-12-31 15:13:01 +0100

    Fancier emoji selection

--- .emacs.d/settings/setup-autocompletion.el
+++ .emacs.d/settings/setup-autocompletion.el
@@ -3,4 +3,9 @@
   :config
   (setq company-dabbrev-downcase nil)
   (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package company-emoji
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-emoji))
 (provide 'setup-autocompletion)
