acd06eac98edbe3bd6395bbfbf5714f66395336c
Horst Gutmann <zerok@zerokspot.com>
2023-08-17 20:39:37 +0200

    wezterm: left-opt as compose key

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,4 +5,5 @@ config.font_size = 13.0
 config.font = wezterm.font('JetBrains Mono')
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
+config.send_composed_key_when_left_alt_is_pressed = true
 return config
